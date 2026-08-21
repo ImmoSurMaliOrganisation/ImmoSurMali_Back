1632607d7d9ca4c37b80c6f1054b606c33d56255
cisse <yamaciss068@gmail.com>
2026-08-21 19:30:20 +0000

    Initialisation du backend de la plateforme immobiliere

---
+++ src/main/java/com/immobilier/plateforme/PlateformeImmobiliereApplication.java
@@ -0,0 +1,13 @@
+package com.immobilier.plateforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeImmobiliereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformeImmobiliereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/immobilier/plateforme/PlateformeImmobiliereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immobilier.plateforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformeImmobiliereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
